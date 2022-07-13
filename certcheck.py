8ee08b23ca9d2aa224e1d09663e9fa8fab5e15bd
Neil Hartsfield <49576227+neilhartsfield@users.noreply.github.com>
2022-07-13 10:44:45 -0500

    Update certcheck.py
    * added 3 new certs from KB87096
    * fixed issue which resulted in the following error on some MERs:
    
    Traceback (most recent call last):
      File "C:/src/certcheckoriginal.py", line 50, in <module>
        contents = fd.read()
      File "C:\Users\HartsfieldNeil\AppData\Local\Programs\Python\Python310\lib\encodings\cp1252.py", line 23, in decode
        return codecs.charmap_decode(input,self.errors,decoding_table)[0]
    UnicodeDecodeError: 'charmap' codec can't decode byte 0x81 in position 95451: character maps to <undefined>

--- certcheck.py
+++ certcheck.py
@@ -17,15 +17,18 @@ blobs = {
     '3679CA35668772304D30A5FB873B0FA77BB70D54': 'VeriSign Universal Root Certification Authority (2037)', 
     '4EB6D578499B1CCF5F581EAD56BE3D9B6744A5E5': 'VeriSign Class 3 Public Primary Certification Authority - G5 (2036)', 
     '8FBE4D070EF8AB1BCCAF2A9D5CCAE7282A2C66B3': 'Microsoft Code Verification Root (2025)', 
-    'B1BC968BD4F49D622AA89A81F2150152A41D829C': 'GlobalSign Root CA (2028)', 
+    'B1BC968BD4F49D622AA89A81F2150152A41D829C': 'GlobalSign Root CA (2028)',
+    '4EFC31460C619ECAE59C1BCE2C008036D94C84B8': 'GlobalSign Code Signing Root R45 (2045)', 
     'D1EB23A46D17D68FD92564C2F1F1601764D8E349': 'AAA Certificate Services (2028)', 
     'D69B561148F01C77C54578C10926DF5B856976AD': 'GlobalSign (2029)', 
     'E12DFB4B41D7D9C32B30514BAC1D81D8385E2D46': 'UTN-USERFirst-Object (2019)', 
     '090D03435EB2A8364F79B78CB173D35E8EB63558': 'GlobalSign CodeSigning CA - SHA256 - G3 (2024)', 
     '0BBFAB97059595E8D1EC48E89EB8657C0E5AAE71': 'GlobalSign (2028)', 
     '17661DFBA03E6AAA09142E012D216864F01D1F5E': 'McAfee Code Signing CA 2 (2024)', 
-    '3BA63A6E4841355772DEBEF9CDCF4D5AF353A297': 'DigiCert SHA2 Assured ID Timestamping CA (2031)', 
-    '495847A93187CFB8C71F840CB7B41497AD95C64F': 'VeriSign Class 3 Code Signing 2010 CA (2020)', 
+    '3BA63A6E4841355772DEBEF9CDCF4D5AF353A297': 'DigiCert SHA2 Assured ID Timestamping CA (2031)',
+    '4C5D80D2CD06B1A493C49B2E9BED4A57C2F873E5': 'GlobalSign Code Signing Root R45 (2029)', 
+    '495847A93187CFB8C71F840CB7B41497AD95C64F': 'VeriSign Class 3 Code Signing 2010 CA (2020)',
+    '7A2146EDB29E2EAD64AFBE7CEAD0B6085D437A32': 'GlobalSign GCC R45 CodeSigning CA 2020 (2030)', 
     '9151B539751B891401C745A9DE301CBDBADF3FB6': 'McAfee OV SSL CA 2 (2024)', 
     'A75AC657AA7A4CDFE5F9DE393E69EFCAB659D250': 'AddTrust External CA Root (2023)', 
     'B69E752BBE88B4458200A7C0F4F5B3CCE6F35B47': 'COMODO RSA Code Signing CA (2028)', 
@@ -46,7 +49,7 @@ with tempfile.TemporaryDirectory() as tmp:
      print('Created temporary directory:', tmp, '\n')
      shutil.unpack_archive(path, tmp)
      cert_text = os.path.join(tmp, '0', 'CMD_PS_DIR_CERT.txt')
-     with open(cert_text) as fd:
+     with open(cert_text,encoding="ANSI") as fd:
         contents = fd.read()
 for item in blobs:
     if item in contents:
